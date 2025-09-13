b2c22bfc8a7343bf9a9a42b7c9ef34eaa306e4d3
tarjan77 <93963117+tarjan77@users.noreply.github.com>
2025-09-12 21:51:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -11,6 +12,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   base: "/shree-cleaning/",
+  build: {
+    outDir: "build",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
